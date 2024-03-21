9e5d454152dec791da18f4336ea2015f94e07235
hplaRno1 <156960009+hplaRno1@users.noreply.github.com>
2024-03-21 02:35:20 +0000

    Quiz 6-1 Question 2

--- main.hpp
+++ main.hpp
@@ -1,3 +1,56 @@
 /***************************************************
  * Code your program here
  ***************************************************/
+ #include  <iostream>
+ #include <fstream>
+ using namespace std;
+
+ int writeFile(string);
+
+ int readFile(string);
+
+ int writeFile(string filename){
+    //code
+    ofstream ofs;
+    ofs.open("employees.txt");
+    if(!ofs){
+        cout << "Failed to create file!" << endl;
+        exit(0);
+    }
+    int Total, id, salary;
+    string name, department;
+    cout << "How many employees are there?: " << endl;
+    cin >> Total;
+    ofs << Total;
+    for(int i = 0; i < Total; i++){
+        cout << "Enter the ID: ";
+        cin >> id;
+        ofs << id << " ";
+        cout << "Enter the employee's name: ";
+        cin >> name;
+        ofs << name << " ";
+        cout << "Enter which department they work in: ";
+        cin >> department;
+        ofs << department << endl;
+        cout << "Enter their salary: ";
+        cin >> salary;
+        ofs << salary << " ";
+    }
+    return Total;
+ }
+
+int readFile(string filename){
+    ifstream ifs;
+    ifs.open("employees.txt");
+    if(!ifs){
+        cout << "Failed to open file!" << endl;
+        exit(0);
+    }
+    int Total, id, salary;
+    string name, department;
+    for (int i = 0; i < Total; i++){
+        cout << "Information: " << endl;
+        ifs >> id >> name >> department >> salary;
+    }
+    return Total;
+}
